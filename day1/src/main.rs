34c7579d6e7c1ff303420561f7d9b273fe02791a
David Simon <D@vidSimon.com>
2018-12-03 00:20:56 -0500

    Part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,14 +1,9 @@
 use std::fs::File;
 use std::io::{BufRead, BufReader, Result};
 use std::env;
+use std::collections::HashSet;
 
-fn main() -> Result<()> {
-    let args: Vec<String> = env::args().collect();
-
-    let filename = &args[1];
-
-    let file = File::open(filename)?;
-
+fn part1(file: File) -> Result<()> {
     let mut frequency: i32 = 0;
     
     for line in BufReader::new(file).lines() {
@@ -18,4 +13,53 @@ fn main() -> Result<()> {
 
     println!("{}", frequency);
     Ok(())
+}
+
+fn part2(file: File) -> Result<()> {
+    let mut changes: Vec<i32> = Vec::new();
+
+    for line in BufReader::new(file).lines() {
+        changes.push(line.unwrap().parse::<i32>().unwrap());
+    }
+
+    let mut freq: i32 = 0;
+    let mut seen: HashSet<i32> = HashSet::new();
+
+    let mut done = false;
+    while !done {
+        for change in &changes {
+            seen.insert(freq);
+            freq += change;
+
+            if seen.contains(&freq) {
+                done = true;
+                break
+            }
+        }
+    }
+
+    println!("{}", freq);
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        3 => {
+                let part = &args[1];
+                let filename = &args[2];
+
+                let file = File::open(filename)?;
+
+                match &part[..] {
+                    "1" => part1(file),
+                    "2" => part2(file),
+                    _ => Ok(())
+                }
+        },
+        _ => Ok(())
+    }
+
+
 }
